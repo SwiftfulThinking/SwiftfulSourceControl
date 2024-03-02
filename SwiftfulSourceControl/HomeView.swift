a318e8241e03df2a0510e166fbfe37b8587371ba
Nick <hello@swiftful-thinking.com>
2024-03-02 17:08:49 -0500

    HomeView updates (progress)

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Screen 2!")
+        ZStack {
+            Text("Screen 2!")
+            
+            Text("Screen 2!")
+            Text("Screen 2!")
+        }
     }
 }
 
