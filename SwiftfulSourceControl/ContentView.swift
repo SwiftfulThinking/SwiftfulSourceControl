a957d21f0b6d5c9e901f9546ce6ee11d01a46442
Nick <hello@swiftful-thinking.com>
2024-03-02 17:21:05 -0500

    [Clean] Reorder commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -11,17 +11,17 @@
  NEW FEATURE:
  [Feature] Description of the feature
  
- BUG IN PRODUCTION:
- [Patch] Description of patch
- 
  BUG NOT IN PRODUCTION:
  [Bug] Description of the bug
  
- MUNDANE TASKS:
- [Clean] Description of changes
- 
  RELEASE:
  [Release] Description of release
+
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+
+ MUNDANE TASKS:
+ [Clean] Description of changes
  
  */
 
