ac01d12b85cf25624607145c80fe2fe3c6c12eeb
Nick <hello@swiftful-thinking.com>
2024-03-02 20:34:47 -0500

    [Feature] Fix one

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -44,7 +44,7 @@ struct ContentView: View {
                         Image(systemName: "magnifyingglass")
                             .font(.largeTitle)
                             .foregroundStyle(.tint)
-                        Text("Swiftful Thinking!!!!!!")
+                        Text("Swiftful Thinking!")
                         
                         Button("Click me!") {
                             
