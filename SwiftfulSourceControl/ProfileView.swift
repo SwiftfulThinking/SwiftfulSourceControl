c874c4824c7fc0cb172da6ed764ea82e2a97ab64
Nick <hello@swiftful-thinking.com>
2024-03-03 13:56:15 -0500

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Nick Sarno on 3/3/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Nick!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
