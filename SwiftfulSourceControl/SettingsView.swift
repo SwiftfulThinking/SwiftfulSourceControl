3e624fa8311a7364e169fb285dc3866d502499c7
Nick <hello@swiftful-thinking.com>
2024-03-02 22:10:41 -0500

    [Feature] Add doSomething state

--- SwiftfulSourceControl/SettingsView.swift
+++ SwiftfulSourceControl/SettingsView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @State private var doSomething: Bool = false
+    
     var body: some View {
-        Text("Settings!")
+        Text("Settings View!")
     }
 }
 
