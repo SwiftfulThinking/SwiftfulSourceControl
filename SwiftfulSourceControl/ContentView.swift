e38ca76daea5e0495beff91ad1021a2f15bbdc73
Nick <hello@swiftful-thinking.com>
2024-03-02 20:12:31 -0500

    [Feature] Add update to content view on scroll view

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -41,7 +41,14 @@ struct ContentView: View {
             ScrollView {
                 VStack {
                     ForEach(0..<20) { _ in
-                        Text("Yo!")
+                        Image(systemName: "globe")
+                            .font(.largeTitle)
+                            .foregroundStyle(.tint)
+                        Text("Some New Title!!")
+            
+                         Button("Click me!") {
+                
+                         
                     }
                 }
             }
