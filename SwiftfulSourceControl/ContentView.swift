5d33c305557043c134acaaf48e9d59ef45da11aa
Nick <hello@swiftful-thinking.com>
2024-03-02 20:35:13 -0500

    [Feature] Subscribe button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -46,7 +46,7 @@ struct ContentView: View {
                             .foregroundStyle(.tint)
                         Text("Swiftful Thinking!")
                         
-                        Button("Click me!") {
+                        Button("Subscribe now!") {
                             
                             
                         }
