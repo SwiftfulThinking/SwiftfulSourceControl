c36fc1e43b52d946e820258d603083cdc4a6ee04
Nick Sarno <44950578+SwiftfulThinking@users.noreply.github.com>
2024-03-02 22:04:50 -0500

    Ns/add push notifications (#2)
    Added push notifs per XYZ
    
    ---------
    
    Co-authored-by: Nick <hello@swiftful-thinking.com>

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -49,9 +49,9 @@ struct ContentView: View {
                         Image(systemName: "globe")
                             .font(.largeTitle)
                             .foregroundStyle(.tint)
-                        Text("Swiftful!!!!!!!")
+                        Text("Swiftful Thinking!!!")
                         
-                        Button("Subscribe now!") {
+                        Button("Subscribe!") {
                             
                             
                         }
