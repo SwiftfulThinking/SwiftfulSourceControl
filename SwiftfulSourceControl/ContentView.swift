5ec6153dfb925bd32351d5c90f15dd22dbc70f8b
Nick <hello@swiftful-thinking.com>
2024-03-02 21:38:30 -0500

    [Clean] Update docs

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -14,8 +14,8 @@
  Pull = Fetch remote commits to local repo
  Merge = Joining two different branch
  Rebase = Moving one branch on top of another branch
- Cherry picking = Duplicating (copying) one commit from one branch to another
- 
+ Cherry Picking = Duplicating (copying) one commit from one branch to another
+ Pull Request (PR) = Request to merge branch
  
  
  COMMIT MESSAGES
