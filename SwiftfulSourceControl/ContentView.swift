22f24c6885de1e987ecfcbc366d71ad2fea78ac4
Nick <hello@swiftful-thinking.com>
2024-03-02 20:25:15 -0500

    [Feature] Update title name

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -44,7 +44,7 @@ struct ContentView: View {
                         Image(systemName: "magnifyingglass")
                             .font(.largeTitle)
                             .foregroundStyle(.tint)
-                        Text("Some New Title!!")
+                        Text("Swiftful Thinking!!!!!!")
                         
                         Button("Click me!") {
                             
