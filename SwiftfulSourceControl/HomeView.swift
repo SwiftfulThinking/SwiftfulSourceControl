5355c1382abb1381daa18fe01dd6ab11986afca1
Nick <hello@swiftful-thinking.com>
2024-03-02 20:35:58 -0500

    [Feature] Fix title

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var title: String = "Hello"
+    @State private var title: String = "Hello, world!"
     
     var body: some View {
         VStack {
