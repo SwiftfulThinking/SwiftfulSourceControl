5735b9c3ef30288f3536292a292d3326eb7454d7
Nick <hello@swiftful-thinking.com>
2024-03-02 21:16:01 -0500

    [Clean] Update documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -7,11 +7,16 @@
 
 /*
  Clone = Copying the repo locally
- Commit = Save ("checkpoint") on our current branch
+ Commit = Save ("checkpoint") changes on our current branch
  Stage = Prepare changes for a commit
  Stash = Save changes for later
  Push = Send local commits to remote repo
  Pull = Fetch remote commits to local repo
+ Merge = Joining two different branch
+ Rebase = Moving one branch on top of another branch
+ Cherry picking = Duplicating (copying) one commit from one branch to another
+ 
+ 
  
  COMMIT MESSAGES
  
