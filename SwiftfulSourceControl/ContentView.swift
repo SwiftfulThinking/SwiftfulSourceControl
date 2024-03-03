35a85d2aedc1b97a266f7284f3f38eef59e87c20
Nick <hello@swiftful-thinking.com>
2024-03-02 19:52:16 -0500

    [Feature] Change icon to house.fill (was heart.fill)

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -38,7 +38,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "heart.fill")
+            Image(systemName: "house.fill")
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Swiftful Thinking!")
