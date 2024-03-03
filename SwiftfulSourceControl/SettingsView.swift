302ba63e3f0728e2950c6c015fa689a946adb1c6
Nick <hello@swiftful-thinking.com>
2024-03-02 21:40:53 -0500

    [Feature] Add SettingsView

---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Nick Sarno on 3/2/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings!")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
