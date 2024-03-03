37fa8fa422252a5d0967cbfe0057febd4b228008
Nick <hello@swiftful-thinking.com>
2024-03-02 22:23:27 -0500

    [Feature] Update Settings text

--- SwiftfulSourceControl/SettingsView.swift
+++ SwiftfulSourceControl/SettingsView.swift
@@ -12,7 +12,7 @@ struct SettingsView: View {
     @State private var doSomething: Bool = false
     
     var body: some View {
-        Text("Settings View!")
+        Text("Settings!")
     }
 }
 
